fa663d366bc2bafaf2aacc0a9176c9d14223ba0d
SunLiveMatrix <152664029+SunLiveMatrix@users.noreply.github.com>
2024-02-01 20:12:03 -0300

    bm
    Codux is an all-in-one visual development environment. Whether you’re starting a new app, creating components, or editing an existing project, every visual change you make reflects instantly in the code and vice versa. To learn more about Codux, visit our website - [https://www.codux.com/](https://www.codux.com/)

---
+++ matrix/gnu/bin/bm.d
@@ -0,0 +1,12 @@
+module matrix.gnu.bin.bm;
+
+import std.stdio;
+
+void values(double x, double y, double z) {
+    return values(x, y, z);
+}
+
+void main(string[] args)
+{
+    writeln("values");
+}
